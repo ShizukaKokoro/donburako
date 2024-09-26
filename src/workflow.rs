9452abeedbda1e80d15536d48030d1e98fffc66a
ShizukaKokoro <107260995+ShizukaKokoro@users.noreply.github.com>
2024-09-26 14:45:49 +0900

    feat(workflow): :sparkles: チェーンでビルドできるよう変更

--- src/workflow.rs
+++ src/workflow.rs
@@ -14,22 +14,28 @@ struct WorkflowBuilder {
     graph: Option<Graph>,
 }
 impl WorkflowBuilder {
-    fn add_node(&mut self, node: NodeBuilder) {
+    fn add_node(self, node: NodeBuilder) -> Self {
         if self.graph.is_some() {
             panic!("Cannot add node after building graph");
         }
-        self.nodes.push(node);
+        let mut nodes = self.nodes;
+        nodes.push(node);
+        Self { nodes, ..self }
     }
 
-    fn finish_nodes(&mut self) {
-        self.graph = Some(Graph::new(self.nodes.len()));
+    fn finish_nodes(self) -> Self {
+        Self {
+            graph: Some(Graph::new(self.nodes.len())),
+            ..self
+        }
     }
 
-    fn add_edge<T: 'static + Send + Sync>(&mut self, from: usize, to: usize) {
+    fn add_edge<T: 'static + Send + Sync>(mut self, from: usize, to: usize) -> Self {
         self.graph.as_mut().unwrap().add_edge(from, to).unwrap();
         let edge = Arc::new(Edge::new::<T>());
         self.nodes[from].add_output(edge.clone());
         self.nodes[to].add_input(edge);
+        self
     }
 
     fn build(self) -> Workflow {
@@ -52,16 +58,16 @@ mod tests {
 
     #[tokio::test]
     async fn test_workflow() {
-        let mut builder = WorkflowBuilder::default();
         let node1 = NodeBuilder::new(Box::new(|_, _| Box::pin(async {})));
         let node2 = NodeBuilder::new(Box::new(|_, _| Box::pin(async {})));
         let node3 = NodeBuilder::new(Box::new(|_, _| Box::pin(async {})));
-        builder.add_node(node1);
-        builder.add_node(node2);
-        builder.add_node(node3);
-        builder.finish_nodes();
-        builder.add_edge::<i32>(0, 1);
-        builder.add_edge::<i32>(1, 2);
+        let builder = WorkflowBuilder::default()
+            .add_node(node1)
+            .add_node(node2)
+            .add_node(node3)
+            .finish_nodes()
+            .add_edge::<i32>(0, 1)
+            .add_edge::<i32>(1, 2);
         let workflow = builder.build();
         assert_eq!(workflow.nodes.len(), 3);
     }
