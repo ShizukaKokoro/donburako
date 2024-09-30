c7688cb615b3d19737985e1e671caf9e208f3f12
ShizukaKokoro <107260995+ShizukaKokoro@users.noreply.github.com>
2024-09-30 13:00:50 +0900

    feat(container): :sparkles: コンテナを実装
    エラーも実装

--- src/container.rs
+++ src/container.rs
@@ -4,7 +4,21 @@
 //! 中に入っているデータの型は、決まっておらず、任意の型を格納し、任意の型を取り出すことができる。
 //! ただし、取り出すデータの型は入れたデータの型と一致している必要がある。
 
+use std::any::{Any, TypeId};
+use thiserror::Error;
 use tokio_util::sync::CancellationToken;
+
+/// コンテナエラー
+#[derive(Debug, Error, PartialEq)]
+pub enum ContainerError {
+    /// コンテナ内の型の不一致
+    #[error("Type is mismatched with the stored data")]
+    TypeMismatch,
+
+    /// コンテナ内にデータが存在しない
+    #[error("Data is not found in the container")]
+    DataNotFound,
+}
 
 /// キャンセルスタック
 #[derive(Default, Clone)]
@@ -36,6 +50,46 @@ impl CancelStack {
         if let Some(token) = token {
             token.cancel();
         }
+    }
+}
+
+/// コンテナ
+///
+/// データを格納するためのコンテナ。
+#[derive(Default)]
+pub struct Container {
+    data: Option<Box<dyn Any + 'static + Send + Sync>>,
+    ty: Option<TypeId>,
+}
+impl Container {
+    /// データを格納する
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - 格納するデータ
+    pub fn store<T: 'static + Send + Sync>(&mut self, data: T) {
+        self.data = Some(Box::new(data));
+        self.ty = Some(TypeId::of::<T>());
+    }
+
+    /// データを取り出す
+    ///
+    /// # Returns
+    ///
+    /// 格納されているデータ
+    pub fn take<T: 'static + Send + Sync>(&mut self) -> Result<T, ContainerError> {
+        if let Some(data) = self.data.take() {
+            if let Some(ty) = self.ty {
+                if ty == TypeId::of::<T>() {
+                    if let Ok(data) = data.downcast::<T>() {
+                        return Ok(*data);
+                    }
+                } else {
+                    return Err(ContainerError::TypeMismatch);
+                }
+            }
+        }
+        Err(ContainerError::DataNotFound)
     }
 }
 
@@ -86,5 +140,50 @@ mod tests {
         stack.cancel();
 
         assert_eq!(stack.0.len(), 0);
+    }
+
+    #[test]
+    fn test_container_init() {
+        let container = Container::default();
+
+        assert!(container.data.is_none());
+        assert!(container.ty.is_none());
+    }
+
+    #[test]
+    fn test_container_store() {
+        let mut container = Container::default();
+        container.store(42);
+
+        assert!(container.data.is_some());
+        assert!(container.ty.is_some());
+    }
+
+    #[test]
+    fn test_container_take() {
+        let mut container = Container::default();
+        container.store(42);
+
+        let data = container.take::<i32>().unwrap();
+        assert_eq!(data, 42);
+    }
+
+    #[test]
+    fn test_container_take_type_mismatch() {
+        let mut container = Container::default();
+        container.store(42);
+
+        let result = container.take::<&str>();
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), ContainerError::TypeMismatch);
+    }
+
+    #[test]
+    fn test_container_take_data_not_found() {
+        let mut container = Container::default();
+
+        let result = container.take::<i32>();
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), ContainerError::DataNotFound);
     }
 }
